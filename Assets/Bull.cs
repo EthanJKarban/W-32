f702b5941abf036404774b74a4b3ca8ea07ebe7b
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2026-02-12 13:20:00 -0800

    Guess who's back, back again.

--- Assets/Bull.cs
+++ Assets/Bull.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class Bull : MonoBehaviour
 {
     Bullet bullet;
 
+    private Vector2 spawnPoint;
     void Start()
     {
         transform.rotation = Quaternion.Euler(0, 0, bullet.rotation);
