26e5eed812323cdcad470748ceeabad89aaa3d66
EthanJKarban <1721000@mukilteo.wednet.edu>
2026-02-26 20:57:38 -0800

    Unmotivated stuff

--- Assets/BM.cs
+++ Assets/BM.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class BM : MonoBehaviour
 {
-     List<GameObject> bullets;
-
-    void Start()
-    {
-        bullets = new List<GameObject>();
-    }
-
+    //public static List<GameObject> bullets;
+    //private void Start()
+    //{
+    //    bullets = new List<GameObject>();
+    //}
     //public static GameObject GetBulletFromPool()
     //{
     //    for (int i = 0; i < bullets.Count; i++)
     //    {
     //        if (!bullets[i].active)
     //        {
-    //            bullets[i].GetComponent<Bull>(). = true;
+    //            var b = bullets[i].GetComponent<Bullet>();
+    //            b.timer = b.lifeTime;
+    //            bullets[i].SetActive(true);
     //            return bullets[i];
     //        }
     //    }
+    //    return null;
+    //}
+    //public static GameObject GetBulletFromPoolWithType(string type)
+    //{
+    //    for (int i = 0; i < bullets.Count; i++)
+    //    {
+    //        if (!bullets[i].active && bullets[i].GetComponent<Bullet>().type == type)
+    //            return bullets[i];
+    //    }
+    //    return null;
     //}
 }
